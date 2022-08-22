ec8eccf88959c3a759a242abc3129ef06c21f5dc
grabofus <grabomobil23@gmail.com>
2022-08-22 21:22:38 +0100

    moved assets

--- src/App.tsx
+++ src/App.tsx
@@ -346,7 +346,7 @@ function App() {
       <div className="spacer"></div>
       <div className="coffee">
         <Button style={{ backgroundColor: 'transparent', padding: 0, borderRadius: 8 }} href="https://www.buymeacoffee.com/grabofus">
-          <img alt="Buy me a coffee" width="200" src="./coffee.png"></img>
+          <img alt="Buy me a coffee" width="200" src="./assets/coffee.png"></img>
         </Button>
       </div>
       <Modal
